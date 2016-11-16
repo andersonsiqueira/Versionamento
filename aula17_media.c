66af2b9d5326fead14e2569bac968e693b070344
andersonsiqueira <siqueirastos@yahoo.com.br>
2016-11-16 21:49:11 -0200

    Atualização 4
    Tratativa para Exame foi acrescentada

--- aula17_media.c
+++ aula17_media.c
@@ -22,24 +22,27 @@ int main() {
 	printf("Informe o nome do aluno     : ");
 	fgets(vNome,sizeof(vNome),stdin);
 	
-	printf("Informe a MÃ©dia 1o Bimestre : ");
+	printf("Informe a Média 1o Bimestre : ");
 	scanf("%f",&nMedia1Bim);
 	
-	printf("Informe a MÃ©dia 2o Bimestre : ");
+	printf("Informe a Média 2o Bimestre : ");
 	scanf("%f",&nMedia2Bim);
 	
-	printf("Informe a MÃ©dia 3o Bimestre : ");
-	scanf("%f",&nMedia3Bim);
+	printf("Informe a Média 3o Bimestre : ");
+	scanf("%f",&nMedia3Bim); 
 	
-	printf("Informe a MÃ©dia 4o Bimestre : ");
+	printf("Informe a Média 4o Bimestre : ");
 	scanf("%f",&nMedia4Bim);
 	
 	nMediaFinal = (nMedia1Bim+nMedia2Bim+nMedia3Bim+nMedia4Bim)/4;
 	
-	printf("\n\nO aluno %s foi estÃ¡",vNome);
+	printf("\n\nO aluno %s está",vNome);
 	
 	if (nMediaFinal >= 6) {
 		printf("\nAprovado");
+	}
+	else if ( (nMediaFinal >= 4) && (nMediaFinal < 6)) {
+		printf("\nem exame");
 	}
 	else {
 		printf("\nReprovado");
